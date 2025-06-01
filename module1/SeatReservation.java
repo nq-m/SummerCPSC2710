15cba99dbb094c1d048ecce895ebe78b69b6c520
N. Mohammad Raza <mrn0035@auburn.edu>
2025-06-01 14:32:35 -0400

    Modified SeatReservation.java

--- module1/SeatReservation.java
+++ module1/SeatReservation.java
@@ -1,3 +1,66 @@
 public class SeatReservation {
+	private String flightDesignator;
+	private java.time.LocalDate flightDate;
+	private String firstName;
+	private String lastName;
+
+public String getFlightDesignator() {
+	return flightDesignator;
+}
+
+public void setFlightDesignator(String fd) {
+	this.flightDesignator = fd;
+}
+public java.time.LocalDate getFlightDate() {
+	return flightDate;
+}
+public void setFlightDate(java.time.LocalDate date) {
+	this.flightDate = date;
+}
+
+public String getFirstName() {
+	return firstName;
+}
+
+public void setFirstName(String fn) {
+	this.firstName = fn;
+}
+
+public String getLastName() {
+	return lastName;
+}
+
+public void setLastName(String ln) {
+	this.lastName = ln;
+}	
+
+public String toString() {
+	String result = "SeatReservation{";
+	if (flightDesignator == null || flightDesignator.trim().isEmpty()) {
+		result += "flightDesignator=null,";
+	} else {
+			result += "flightDesignator=" + flightDesignator + ",";
+	}
+	if (flightDate == null) {
+		result += "flightDate=null,";
+	} else {
+			result += "flightDate=" + flightDate + ",";
+	}
+
+	if (firstName == null || firstName.trim().isEmpty()) {
+		result += "firstName=null,";
+	} else {
+			result += "firstName=" + firstName + ",";	
+	}
+			
+	if (lastName == null || lastName.trim().isEmpty()) {
+	result += "lastName=null";
+	} else {
+	result += "lastName=" + lastName;	
+	}
+	result += "}";
+	return result;
+}
 	
+
 }
